d4412958e63400a3c41c8819afa208cca50d0992
HANT666 <iywppfw@yandex.ru>
2017-04-16 01:09:07 +0500

    добавление анекдота

--- script.js
+++ script.js
@@ -58,31 +58,53 @@
       Anekdot.addTag(tag);
     }
 
-    api().ask({method: 'tag.all'}).try(function(response) {
+    api().ask({ method: 'tag.all' }).try(function (response) {
       $('ul.list.aside.tags').clear();
-      response.map(({name}) => $('ul.list.aside.tags').add('li{'+name+'}'));
+      response.map(({ name }) => $('ul.list.aside.tags').add('li{' + name + '}'));
     });
 
-    api().ask({method: 'anekdot.all'}).try(function(response) {
+    api().ask({ method: 'anekdot.all' }).try(function (response) {
       $('ul.list.aside.anekdots').clear();
-      response.map(({caption, id}) => { 
-        $('ul.list.aside.anekdots').add('li{'+caption+'}');
+      response.map(({ caption, id }) => {
+        $('ul.list.aside.anekdots').add('li{' + caption + '}');
+        /** @todo выборка только что добавленого li */
         var list = $('ul.list.aside.anekdots').find(['li']);
-        var item = list.q(list.length-1);
-        item.on({ click: function(event) { loadAnekdot(id) } });
+        var item = list.q(list.length - 1);
+        item.on({ click: function (event) { loadAnekdot(id) } });
       });
     });
+
+    /** добавляет событие сохранения анекдота */
+    $('form#editor').on({
+      submit: function (e) {
+        saveAnekdot(getName(), 100, getText(), getName()); alert("Анекдот добавлен"); return false;
+      }
+    })
+    /** получает название анекдота из формы */
+    function getName() {
+      return $('input[name=name]')[0].value;
+    }
+    /** получает текст анекдота из формы */
+    function getText() {
+      return $('textarea')[0].value;
+    }
+    /** сохраняет анекдот */
+    function saveAnekdot(_caption, _number, _text, _name) {
+      api().ask({ method: 'anekdot.add', caption: _caption, number: _number, text: _text, name: _name })
+        .try(function (response) {
+        });
+    }
 
     loadAnekdot(12);
 
     function loadAnekdot(ID) {
-      api().ask({method: 'anekdot.get', id: ID}).try(function(response) {
+      api().ask({ method: 'anekdot.get', id: ID }).try(function (response) {
         $('#anekdot>h2', '#anekdot>div').clear();
         $('#anekdot>h2').html(response.caption);
         $('#anekdot>div').html(response.version[0].text);
       });
     }
-    
+
   });
 
   function api() {
